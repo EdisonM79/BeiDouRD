dc7663ca4b2ba7bb71be5d6b260daf2dcf75901d
EdisonM79 <mopengxi@163.com>
2019-07-10 15:50:48 +0800

    添加了对波束功率的解析和显示

--- app/src/main/java/com/cdjzsk/rd/beidourd/MainActivity.java
+++ app/src/main/java/com/cdjzsk/rd/beidourd/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cdjzsk.rd.beidourd.adapter.ContactsAdapter;
@@ -19,6 +21,8 @@ import com.jzsk.seriallib.SerialClient;
 import com.jzsk.seriallib.SupportProtcolVersion;
 import com.jzsk.seriallib.conn.MessageListener;
 import com.jzsk.seriallib.msg.BaseMessage;
+import com.jzsk.seriallib.msg.msgv21.Message;
+import com.jzsk.seriallib.util.ArrayUtils;
 import com.jzsk.seriallib.util.LogUtils;
 
 import java.util.ArrayList;
@@ -30,25 +34,60 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements ClientStateCallback {
 
 	private static final String TAG = LogUtils.makeTag(MainActivity.class);
-//	@BindView(R.id.textView)
-//	TextView mTextView;
-//	@BindView(R.id.btn_regsiter)
-//	Button mBtnRegsiter;
-//	@BindView(R.id.btn_connect)
-//	Button mBtnConnect;
-//	@BindView(R.id.btn_close)
-//	Button mBtnClose;
-//	@BindView(R.id.btn_select)
-//	Button mBtnSelect;
+	@BindView(R.id.cardId)
+	TextView cardId;
+	@BindView(R.id.frequency)
+	TextView frequency;
 	@BindView(R.id.contacts)
 	ListView listView;//ListView组件
+	@BindView(R.id.bs_tv_0)
+	TextView bsiTv_0;
+	@BindView(R.id.bs_tv_1)
+	TextView bsiTv_1;
+	@BindView(R.id.bs_tv_2)
+	TextView bsiTv_2;
+	@BindView(R.id.bs_tv_3)
+	TextView bsiTv_3;
+	@BindView(R.id.bs_tv_4)
+	TextView bsiTv_4;
+	@BindView(R.id.bs_tv_5)
+	TextView bsiTv_5;
+	@BindView(R.id.bs_tv_6)
+	TextView bsiTv_6;
+	@BindView(R.id.bs_tv_7)
+	TextView bsiTv_7;
+	@BindView(R.id.bs_tv_8)
+	TextView bsiTv_8;
+	@BindView(R.id.bs_tv_9)
+	TextView bsiTv_9;
+	@BindView(R.id.vertical_progressbar0)
+	ProgressBar progressBar0;
+	@BindView(R.id.vertical_progressbar1)
+	ProgressBar progressBar1;
+	@BindView(R.id.vertical_progressbar2)
+	ProgressBar progressBar2;
+	@BindView(R.id.vertical_progressbar3)
+	ProgressBar progressBar3;
+	@BindView(R.id.vertical_progressbar4)
+	ProgressBar progressBar4;
+	@BindView(R.id.vertical_progressbar5)
+	ProgressBar progressBar5;
+	@BindView(R.id.vertical_progressbar6)
+	ProgressBar progressBar6;
+	@BindView(R.id.vertical_progressbar7)
+	ProgressBar progressBar7;
+	@BindView(R.id.vertical_progressbar8)
+	ProgressBar progressBar8;
+	@BindView(R.id.vertical_progressbar9)
+	ProgressBar progressBar9;
+
 
 
 	private SerialClient mSerialClient;
 	private MyDataBaseHelper dbHelper;
 	private MyDataHander myDataHander;
 	private byte[] ICA = "CCICA,0,00".getBytes();
-	private byte[] BSI = "$CCRMO,BSI,2,2*24".getBytes();
+	private byte[] BSI = "CCRMO,BSI,2,2".getBytes();
 
 	private List<Contact> contacts = new ArrayList<Contact>();//存储数据
 	private ContactsAdapter contactsAdapter;//ListView的数据适配器
@@ -59,16 +98,8 @@ public class MainActivity extends AppCompatActivity implements ClientStateCallba
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
-//		mTextView.setMovementMethod(ScrollingMovementMethod.getInstance());
 		myDataHander = new MyDataHander(this);
-		//创建一个数据库助手类
-//		dbHelper = new MyDataBaseHelper(this, "BDStore.db", null, 1); // 执行这句并不会创建数据库文件
-		//获取数据库输入对象，对于增删改都可以用db.execSQL(String sql);  来执行sql语句
-		//db.execSQL("insert into book(name , author, pages, price) values(\"Android数据库操作指南\", \"panda fang\", 200, 35.5)");
-		//遇到字符串要转义 有没有觉得很蛋疼， 用下面的方法就好多了
-		//db.execSQL("insert into book(name , author, pages, price) values(?, ? ,? ,? )", new String[]{"Android数据库操作指南", "panda fang", "200", "35.5"});
-//		SQLiteDatabase db =  dbHelper.getWritableDatabase();
-
+		initSerialClient();
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -80,6 +111,14 @@ public class MainActivity extends AppCompatActivity implements ClientStateCallba
 			}
 		});
 		initContact();
+		byte[] sendMsgICA = ArrayUtils.concatenate(new byte[]{'$'}, ICA, new byte[]{'*'}, ArrayUtils.bytesToHexString(new byte[]{ArrayUtils.xorCheck(ICA)}).getBytes(), new byte[]{0x0D, 0x0A});
+		Message msgICA = new Message(sendMsgICA);
+		mSerialClient.sendMessage(msgICA);
+		//打开波束功率输出
+		byte[] sendMsgBSI = ArrayUtils.concatenate(new byte[]{'$'}, BSI, new byte[]{'*'}, ArrayUtils.bytesToHexString(new byte[]{ArrayUtils.xorCheck(BSI)}).getBytes(), new byte[]{0x0D, 0x0A});
+		Message msgBSI = new Message(sendMsgBSI);
+		mSerialClient.sendMessage(msgBSI);
+
 	}
 	public void initContact(){
 		//User user = new User("655326","Json");
@@ -137,7 +176,9 @@ public class MainActivity extends AppCompatActivity implements ClientStateCallba
 				{
 					String[] msgList = msg.split(",");
 					String carId = msgList[1];
-					String frequency = msgList[5];
+					String freqy = msgList[5];
+					cardId.setText(carId);
+					frequency.setText(freqy);
 				}
 				if(msg.contains("BDFKI"))
 				{
@@ -146,6 +187,32 @@ public class MainActivity extends AppCompatActivity implements ClientStateCallba
 				if(msg.contains("BDTXR"))
 				{
 					String[] msgList = msg.split(",");
+
+				}
+				if(msg.contains("BDBSI"))
+				{
+					String[] bsiList = msg.split(",");
+					bsiTv_0.setText(bsiList[3]);
+					progressBar0.setProgress(Integer.parseInt(bsiList[3]));
+					bsiTv_1.setText(bsiList[4]);
+					progressBar1.setProgress(Integer.parseInt(bsiList[4]));
+					bsiTv_2.setText(bsiList[5]);
+					progressBar2.setProgress(Integer.parseInt(bsiList[5]));
+					bsiTv_3.setText(bsiList[6]);
+					progressBar3.setProgress(Integer.parseInt(bsiList[6]));
+					bsiTv_4.setText(bsiList[7]);
+					progressBar4.setProgress(Integer.parseInt(bsiList[7]));
+					bsiTv_5.setText(bsiList[8]);
+					progressBar5.setProgress(Integer.parseInt(bsiList[8]));
+					bsiTv_6.setText(bsiList[9]);
+					progressBar6.setProgress(Integer.parseInt(bsiList[9]));
+					bsiTv_7.setText(bsiList[10]);
+					progressBar7.setProgress(Integer.parseInt(bsiList[10]));
+					bsiTv_8.setText(bsiList[11]);
+					progressBar8.setProgress(Integer.parseInt(bsiList[11]));
+					String last = bsiList[12].substring(0,1);
+					bsiTv_9.setText(last);
+					progressBar9.setProgress(Integer.parseInt(last));
 
 				}
 			}
@@ -182,34 +249,4 @@ public class MainActivity extends AppCompatActivity implements ClientStateCallba
 		setTxt("串口连接关闭");
 		Toast.makeText(this, "已关闭连接", Toast.LENGTH_SHORT).show();
 	}
-
-//	@OnClick({R.id.btn_regsiter,R.id.btn_connect, R.id.btn_close, R.id.btn_select})
-//	public void onViewClicked(View view) {
-//		switch (view.getId()) {
-//			case R.id.btn_regsiter:
-//				if(mSerialClient != null && mSerialClient.isConnected()) {
-//					byte[] sendMsg = ArrayUtils.concatenate(new byte[]{'$'},ICA,new byte[]{'*'},ArrayUtils.bytesToHexString(new byte[]{ArrayUtils.xorCheck(ICA)}).getBytes(),new byte[]{0x0D,0x0A});
-//					Message msg  = new Message(sendMsg);
-//					mSerialClient.sendMessage(msg);
-//					long currentTime = System.currentTimeMillis();
-//					String timeNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(currentTime);
-//					com.cdjzsk.rd.beidourd.data.entity.Message bdmessage = new com.cdjzsk.rd.beidourd.data.entity.Message("234","345","sfksaj",timeNow);
-//					myDataHander.addMessage(bdmessage);
-//					setTxt("写入成功:" + msg.toString());
-//				}
-//				break;
-//			case R.id.btn_connect:
-//				initSerialClient();
-//				break;
-//			case R.id.btn_close:
-//				closeSerialClient();
-//				break;
-//			case R.id.btn_select:
-//				List<com.cdjzsk.rd.beidourd.data.entity.Message> messageList = myDataHander.getScrollMessageBySendIdOrReceiveId("234",0,5);
-//				for (int i =0; i< messageList.size(); i++)
-//				{
-//					setTxt(messageList.get(i).toString());
-//				}
-//		}
-//	}
 }
