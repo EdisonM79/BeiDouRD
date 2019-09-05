ea1f7ecd537d6023aabebfc02a6845210d829a15
EdisonM79 <mopengxi@163.com>
2019-09-05 18:14:06 +0800

    修改A设备的串口地址

--- seriallib/src/main/java/com/jzsk/seriallib/ClientConstants.java
+++ seriallib/src/main/java/com/jzsk/seriallib/ClientConstants.java
@@ -11,7 +11,7 @@ public class ClientConstants {
   /** L600 */
   //public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyMT0";
   /** AIS */
-  public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyS0";
+  public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyS4";
   /** 军用平板 */
   //public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyMAX1";
   public static final int    PREF_DEFAULT_BAUDRATE   = 115200;
