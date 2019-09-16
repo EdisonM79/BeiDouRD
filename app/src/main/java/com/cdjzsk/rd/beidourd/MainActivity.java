671c6e1fd5489a439c0a8dfc3603cafc92012432
EdisonM79 <mopengxi@163.com>
2019-09-16 11:05:15 +0800

    添加波束功率UI设置

--- app/src/main/java/com/cdjzsk/rd/beidourd/MainActivity.java
+++ app/src/main/java/com/cdjzsk/rd/beidourd/MainActivity.java
@@ -113,6 +113,10 @@ public class MainActivity extends AppCompatActivity {
 		sosButton.setClickable(false);
 
 		initHistogram();
+		//在测试模式下，查看博士功率的UI
+		if (Constant.TEST_UI_MODEL) {
+			initMultiGroupHistogramView("$BDBSI,00,00,1,3,2,4,0,3,1,4,2,1*5E");
+		}
 	}
 
 	@Override
