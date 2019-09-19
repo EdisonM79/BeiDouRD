8c8ef2bafed231ada9327750e1e1ce5c732a04ca
EdisonM79 <mopengxi@163.com>
2019-09-19 17:33:46 +0800

    Ais

--- seriallib/src/main/java/com/jzsk/seriallib/ClientConstants.java
+++ seriallib/src/main/java/com/jzsk/seriallib/ClientConstants.java
@@ -9,9 +9,9 @@ public class ClientConstants {
   /** 异常测试 */
   //public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyMT9";
   /** L600 */
-  public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyMT0";
+  //public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyMT0";
   /** AIS */
-  //public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyS4";
+  public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyS4";
   /** 军用平板 */
   //public static final String PREF_DEFAULT_DEVICE      = "/dev/ttyMAX1";
   public static final int    PREF_DEFAULT_BAUDRATE   = 115200;
